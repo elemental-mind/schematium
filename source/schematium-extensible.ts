9534884bd349fbfb29574d84e0c110346b0e8ea4
Magnus Meseck <magnus.meseck@googlemail.com>
2026-06-25 21:55:27 +0200

    API: Introduce templating customization API
    This API allows users to provide own base classes and inject their own interfaces.

---
+++ source/schematium-extensible.ts
@@ -0,0 +1,16 @@
+// Factory for creating custom API instances with BaseClass injection into the class hierarchy
+export { GenerateTemplatingAPI } from "./templating.ts";
+
+// Definition API interfaces for implementing custom fluent definition chains
+export type {
+    ValueTemplateAPI,
+    OptionalityDefinitionAPI,
+    ValueDefinitionAPI,
+    CollectionDefinitionAPI,
+    TypedCollectionDefinitionAPI,
+    TemplateObject,
+    TemplateObjectEntry,
+    ValueConfiguration,
+    EntryValidationClosure,
+    TemplatingAPI
+} from "./templating.ts";
