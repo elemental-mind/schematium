dcef1ae42bc8529bf3d3a9eea2561d6a08910fcd
Magnus Meseck <magnus.meseck@googlemail.com>
2026-07-04 20:10:11 +0200

    api: update exports for lib users

--- source/schematium.ts
+++ source/schematium.ts
@@ -1,2 +1,2 @@
-export type { TemplateObject, InferSchemaType } from "./templating.ts";
-export { array, arrayOf, list, listOf, boolean, string, number, object, oneOf, schema, valueOf } from "./templating.ts";
+export type { TemplateObject, ValidationAPI, TemplateAPI, ValidationTolerances, ValidationSettings, InferSchemaType } from "./templating.ts";
+export { array, arrayOf, record, recordOf, boolean, string, number, object, oneOf, schema, valueOf } from "./templating.ts";
